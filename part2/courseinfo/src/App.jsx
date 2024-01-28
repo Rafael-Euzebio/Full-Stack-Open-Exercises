ba3c6d1af381336801f7f291eca899254ab70500
Rafael Euzebio <rafaeleuzebiomendes@protonmail.com>
2024-01-28 13:01:03 -0300

    2 - courseinfo - Moving Course component to its own file

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -1,58 +1,6 @@
+import Course from "./components/Course"
+
 const Header = ({ text }) => <h1>{text}</h1>
-
-const CourseHeader = ({ name }) => <h2>{name}</h2>
-
-const Part = ({ part }) => {
-    return (
-        <p>
-            {part.name} {part.exercises}
-        </p>
-    )
-}
-
-const Content = ({ parts }) => {
-    return (
-        <>
-            {
-                parts.map((part) => {
-                    return(
-                        <Part key={part.id} part={part} /> 
-                    )
-                })
-            }
-        </>
-    )
-}
-
-const Total = ({parts}) => {
-    var total = parts.reduce((sum, part) => {
-        return {exercises: sum.exercises + part.exercises}
-    })
-
-    return (
-        <strong>total of {total.exercises} exercises</strong>
-    )
-}
-
-const Course = ({courses}) => {
-
-    return (
-        <>
-            { 
-            courses.map(course => {
-                return (
-                    <div key={course.id}>
-                        <CourseHeader name={course.name} />
-                        <Content parts={course.parts} />
-                        <Total parts={course.parts} />
-                    </div>
-                )
-            }) 
-            }
-        </>
-    )
-}
-
 
 const App = () => {
     const courses = [
---
+++ part2/courseinfo/src/components/Course.jsx
@@ -0,0 +1,55 @@
+const CourseHeader = ({ name }) => <h2>{name}</h2>
+
+const Part = ({ part }) => {
+    return (
+        <p>
+            {part.name} {part.exercises}
+        </p>
+    )
+}
+
+const Content = ({ parts }) => {
+    return (
+        <>
+            {
+                parts.map((part) => {
+                    return(
+                        <Part key={part.id} part={part} /> 
+                    )
+                })
+            }
+        </>
+    )
+}
+
+const Total = ({parts}) => {
+    var total = parts.reduce((sum, part) => {
+        return {exercises: sum.exercises + part.exercises}
+    })
+
+    return (
+        <strong>total of {total.exercises} exercises</strong>
+    )
+}
+
+const Course = ({courses}) => {
+
+    return (
+        <>
+            { 
+            courses.map(course => {
+                return (
+                    <div key={course.id}>
+                        <CourseHeader name={course.name} />
+                        <Content parts={course.parts} />
+                        <Total parts={course.parts} />
+                    </div>
+                )
+            }) 
+            }
+        </>
+    )
+}
+
+export default Course
+
