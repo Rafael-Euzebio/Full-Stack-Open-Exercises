44e0b630a9574f76bffc84926a85e52e50fb2ff5
Rafael Euzebio <rafaeleuzebiomendes@protonmail.com>
2024-02-05 15:49:59 -0300

    using relative path for backend

--- part2/phonebook/src/services/phonebookServices.js
+++ part2/phonebook/src/services/phonebookServices.js
@@ -1,25 +1,25 @@
-import axios from "axios";
-const baseUrl = "http://localhost:3001/persons/";
+import axios from 'axios'
+const baseUrl = '/api/persons'
 
 const getAll = () => {
-  const request = axios.get(baseUrl);
-  return request.then((response) => response.data);
-};
+  const request = axios.get(baseUrl)
+  return request.then((response) => response.data)
+}
 
 const create = (personObject) => {
-  const request = axios.post(baseUrl, personObject);
-  return request.then((response) => response.data);
-};
+  const request = axios.post(baseUrl, personObject)
+  return request.then((response) => response.data)
+}
 
 const remove = (id) => {
-  const request = axios.delete(`${baseUrl}${id}`);
-  return request.then((response) => response.data);
-};
+  const request = axios.delete(`${baseUrl}${id}`)
+  return request.then((response) => response.data)
+}
 
 const update = (personObject) => {
-  const { id } = personObject;
-  const request = axios.put(`${baseUrl}${id}`, personObject);
-  return request.then((response) => response.data);
-};
+  const { id } = personObject
+  const request = axios.put(`${baseUrl}${id}`, personObject)
+  return request.then((response) => response.data)
+}
 
-export default { getAll, create, remove, update };
+export default { getAll, create, remove, update }
