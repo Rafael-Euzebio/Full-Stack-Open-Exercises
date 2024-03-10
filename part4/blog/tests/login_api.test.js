02afd50934571a9bdf7fdb0df6830992d5e99aad
Rafael Euzebio <rafaeleuzebiomendes@protonmail.com>
2024-03-09 21:03:12 -0300

    checking if post request with correct username and password returns a valid jwt token

--- part4/blog/tests/login_api.test.js
+++ part4/blog/tests/login_api.test.js
@@ -4,6 +4,8 @@ const api = supertest(app)
 const bcrypt = require('bcrypt')
 const helper = require('./test_helper')
 const logger = require('../utils/logger')
+const jwt = require('jsonwebtoken')
+require('dotenv').config()
 const mongoose = require('mongoose')
 const User = require('../models/user')
 
@@ -21,6 +23,16 @@ beforeEach(async () => {
   await Promise.all(promiseArray)
 })
 
+test('POST to login returns a valid JWT token if username and password are correct', async () => {
+  const { username, password } = helper.initialUsers[0]
+  const response = await api.post('/api/login').send({ username, password })
+
+  expect(response.body).toHaveProperty('token')
+
+  const tokenVerification = jwt.verify(response.body.token, process.env.JWT_SECRET)
+  expect(tokenVerification).toHaveProperty('username')
+  expect(tokenVerification).toHaveProperty('id')
+})
 
 afterAll(async () => {
   await mongoose.connection.close()
