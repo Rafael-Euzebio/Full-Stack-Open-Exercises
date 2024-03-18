b22ee5e10c96671d87479fcae3c7105630816acc
Rafael Euzebio <rafaeleuzebiomendes@protonmail.com>
2024-03-18 18:13:25 -0300

    removing _id field from initialBlogs

--- part4/blog/tests/test_helper.js
+++ part4/blog/tests/test_helper.js
@@ -4,7 +4,6 @@ const bcrypt = require('bcrypt')
 
 const initialBlogs = [
   {
-    _id: '5a422a851b54a676234d17f7',
     title: 'React patterns',
     author: 'Michael Chan',
     url: 'https://reactpatterns.com/',
@@ -12,7 +11,6 @@ const initialBlogs = [
     __v: 0
   },
   {
-    _id: '5a422bc61b54a676234d17fc',
     title: 'Type wars',
     author: 'Robert C. Martin',
     url: 'http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html',
@@ -20,7 +18,6 @@ const initialBlogs = [
     __v: 0
   },
   {
-    _id: '5a422aa71b54a676234d17f8',
     title: 'Go To Statement Considered Harmful',
     author: 'Edsger W. Dijkstra',
     url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
@@ -28,7 +25,6 @@ const initialBlogs = [
     __v: 0
   },
   {
-    _id: '5a422ba71b54a676234d17fb',
     title: 'TDD harms architecture',
     author: 'Robert C. Martin',
     url: 'http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html',
@@ -36,7 +32,6 @@ const initialBlogs = [
     __v: 0
   },
   {
-    _id: '5a422b3a1b54a676234d17f9',
     title: 'Canonical string reduction',
     author: 'Edsger W. Dijkstra',
     url: 'http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html',
@@ -44,7 +39,6 @@ const initialBlogs = [
     __v: 0
   },
   {
-    _id: '5a422b891b54a676234d17fa',
     title: 'First class tests',
     author: 'Robert C. Martin',
     url: 'http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll',
@@ -55,19 +49,16 @@ const initialBlogs = [
 
 const initialUsers = [
   {
-    _id: '65db9b03311501d8198cf483',
     name: 'Obi-Wan Kenobi',
     username: 'heIsMe',
     password: 'anElegantWeaponForaMoreCivilizedTime'
   },
   {
-    _id: '65db9b03311501d8198cf482',
     name: 'Artoo Detoo',
     username: 'R2-D2',
     password: 'binaryLanguage'
   },
   {
-    _id: '65db9b03311501d8198cf481',
     name: 'Anakin Skywalker',
     username: 'Darth Vader',
     password: 'iAmYourFather'
